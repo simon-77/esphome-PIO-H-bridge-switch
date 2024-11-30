cbf584c05a054d8706b40415d42c6dc1775f6565
simon-77 <30923077+simon-77@users.noreply.github.com>
2024-11-30 17:48:14 +0100

    try to add gpio

--- components/pio_hbridge-switch/switch.py
+++ components/pio_hbridge-switch/switch.py
@@ -7,10 +7,13 @@ empty_switch_ns = cg.esphome_ns.namespace('empty_switch')
 EmptySwitch = empty_switch_ns.class_('EmptySwitch', switch.Switch, cg.Component)
 
 CONFIG_SCHEMA = switch.SWITCH_SCHEMA.extend({
+    cv.Required(CONF_PIN): pins.gpio_output_pin_schema,
     cv.GenerateID(): cv.declare_id(EmptySwitch)
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
+    pin = cg.gpio_pin_expression(config[CONF_PIN])
+    cg.add(var.set_pin(pin))
     yield cg.register_component(var, config)
     yield switch.register_switch(var, config)
