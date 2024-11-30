ee1f56884efcda94fda3a99f1b02b42b50bef5d1
simon-77 <30923077+simon-77@users.noreply.github.com>
2024-11-30 18:08:47 +0100

    make to_code function asynchronous and await component and switch registration

--- components/pio_hbridge-switch/switch.py
+++ components/pio_hbridge-switch/switch.py
@@ -4,17 +4,20 @@ from esphome import pins
 from esphome.components import switch
 from esphome.const import CONF_ID, CONF_PIN
 
+# Define the namespace and class
 empty_switch_ns = cg.esphome_ns.namespace('empty_switch')
 EmptySwitch = empty_switch_ns.class_('EmptySwitch', switch.Switch, cg.Component)
 
+# Configuration schema
 CONFIG_SCHEMA = switch.SWITCH_SCHEMA.extend({
     cv.Required(CONF_PIN): pins.gpio_output_pin_schema,
     cv.GenerateID(): cv.declare_id(EmptySwitch)
 }).extend(cv.COMPONENT_SCHEMA)
 
-def to_code(config):
+# Convert configuration to code
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    pin = cg.gpio_pin_expression(config[CONF_PIN])
+    pin = await cg.gpio_pin_expression(config[CONF_PIN])  # Await the coroutine
     cg.add(var.set_pin(pin))
-    cg.register_component(var, config)
-    switch.register_switch(var, config)
+    await cg.register_component(var, config)  # Await registration
+    await switch.register_switch(var, config)  # Await switch registration
